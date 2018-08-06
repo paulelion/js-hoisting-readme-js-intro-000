82140ba7bf88066cc48b8fe9ee49d38ecb62407a
Paul Elion <paul.j.elion@gmail.com>
2018-08-06 23:49:39 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,10 +1,11 @@
+var lyric = "maybe";
+
 function callMe() {
+  return lyric;
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
 }
 
 
@@ -21,10 +22,10 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
-
   function sayMy() {
     console.log(name);
     var name = "Kristin";
   }
+
+  sayMy();
 }
